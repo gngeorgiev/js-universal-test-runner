f8c9abd8075c0b36a513b1cf3688bc4b25e8eaa0
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-08-15 13:11:23 +0300

    Fixed indentation

--- injectables/test-runner/testRunner.js
+++ injectables/test-runner/testRunner.js
@@ -93,7 +93,7 @@ const MobileTapReporter = require('./mobileTapReporter');
                 if (script.src.indexOf('testRunner') !== -1) {
                     var src = script.src;
                     self.path = src.substr(0, src.lastIndexOf('/') + 1);
-					self.libsPath = self.path.replace('bundles', 'test-libs');
+                    self.libsPath = self.path.replace('bundles', 'test-libs');
                     scripts = script.dataset.js && script.dataset.js.split(',');       
                     scripts.unshift('../../dist/kinvey-html5-sdk.min.js');
                     css = script.dataset.css && script.dataset.css.split(',');
